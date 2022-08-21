3c89adb0735f7f856f3e2d299bddc949f795f4c3
Henri Gouttard <henri.gouttard@protonmail.com>
2022-08-21 13:08:43 +0200

    Popup Tooltip and route ok

--- components/BikeMap/BikeMap.tsx
+++ components/BikeMap/BikeMap.tsx
@@ -5,9 +5,12 @@ import {
   CircleMarker,
   Popup,
   Tooltip,
+  useMap,
+  useMapEvent,
 } from 'react-leaflet'
-import { LatLngTuple, PathOptions } from 'leaflet'
+import { LatLngTuple, PathOptions, Point } from 'leaflet'
 
+import styled from 'styled-components'
 import styles from './BikeMap.module.css'
 import 'leaflet/dist/leaflet.css'
 
@@ -15,6 +18,7 @@ import { Post } from '../../types/post'
 import { ReactNode, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 import Image from 'next/image'
+import { builtinModules } from 'module'
 
 const homePoint: LatLngTuple = [46.167071701158854, 9.031604235079925]
 
@@ -26,10 +30,25 @@ const getPoints = (post: Post) => {
   return points
 }
 
+const initialZoom = 7
+
 const pathOptions: PathOptions = {
   color: 'red',
-  weight: 5,
+  weight: 6,
   fillOpacity: 1,
+}
+
+const markerMobileOptions: PathOptions = {
+  weight: 30,
+  fillColor: 'red',
+  fillOpacity: 1,
+  opacity: 0,
+}
+
+const markerDesktopOptions: PathOptions = {
+  fillColor: 'red',
+  fillOpacity: 1,
+  opacity: 0,
 }
 
 type BikeMapProps = {
@@ -41,40 +60,74 @@ type CheckpointProps = {
   children: ReactNode
 }
 
+const StyledTooltip = styled(Tooltip)`
+  display: flex;
+  flex-direction: column;
+  img {
+    border-radius: 3px;
+  }
+`
+
+const StyledPopup = styled(Popup)`
+  display: flex;
+  flex-direction: column;
+  img {
+    border-radius: 3px;
+    position: relative;
+  }
+  .leaflet-popup-content-wrapper {
+    border-radius: 5px;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+  }
+  .leaflet-popup-content {
+    margin: 0.3rem;
+  }
+  .leaflet-popup-tip {
+    display: none;
+  }
+`
+
 const Checkpoint = ({ center, children }: CheckpointProps) => {
-  const [radius, setRadius] = useState(5)
   const isDesktop = useMediaQuery({ query: '(min-width: 1224px)' })
+  const [radius, setRadius] = useState(10)
+
   return (
     <CircleMarker
       center={center}
-      pathOptions={pathOptions}
+      pathOptions={isDesktop ? markerDesktopOptions : markerMobileOptions}
       radius={radius}
       eventHandlers={{
         mouseover: () => {
-          setRadius(7)
+          setRadius(13)
         },
         mouseout: () => {
-          setRadius(5)
+          setRadius(10)
         },
       }}
     >
       {!isDesktop ? (
-        <Popup className={styles.popup}>{children}</Popup>
+        <StyledPopup
+          closeButton={false}
+          offset={new Point(0, 2)}
+          className={styles.popup}
+        >
+          {children}
+        </StyledPopup>
       ) : (
-        <Tooltip>{children}</Tooltip>
+        <StyledTooltip>{children}</StyledTooltip>
       )}
     </CircleMarker>
   )
 }
 
-const BikeMap = ({ posts }: BikeMapProps) => {
+const MapContent = ({ posts }: BikeMapProps) => {
+  const [zoom, setZoom] = useState(initialZoom)
+  const map = useMapEvent('zoom', () => {
+    setZoom(map.getZoom())
+  })
+
   return (
-    <MapContainer
-      className={styles.map}
-      center={homePoint}
-      zoom={7}
-      scrollWheelZoom={false}
-    >
+    <div className={styles.mapContent}>
       <TileLayer url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png" />
       {posts.map((post) => {
         const points = getPoints(post)
@@ -86,15 +139,32 @@ const BikeMap = ({ posts }: BikeMapProps) => {
               smoothFactor={4}
               positions={points}
             />
-            <Checkpoint center={points[0]}>
-              {post.title}
-              {isImagePresent && (
-                <Image src={post.coverImage} width={300} height={300} />
-              )}
-            </Checkpoint>
+            {zoom > 6 && (
+              <Checkpoint center={points[0]}>
+                {isImagePresent && (
+                  <Image src={post.coverImage} width={300} height={300} />
+                )}
+                {post.title}
+              </Checkpoint>
+            )}
           </div>
         )
       })}
+    </div>
+  )
+}
+
+const BikeMap = ({ posts }: BikeMapProps) => {
+  const firstPostPoints = posts[0].points || []
+  const firstPoint = firstPostPoints[0]
+  return (
+    <MapContainer
+      className={styles.map}
+      center={firstPoint}
+      zoom={initialZoom}
+      scrollWheelZoom={false}
+    >
+      <MapContent posts={posts} />
     </MapContainer>
   )
 }
