ea88624d0fa6d78ad7fb4f8a679306ca9a02cc7e
Henri Gouttard <henri.gouttard@protonmail.com>
2022-08-31 00:52:29 +0200

    Stat base

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,11 +1,49 @@
+import { ReactNode } from 'react'
+import { ButtonLink } from '../components/Buttons/Buttons'
+import { ListIcon, MapIcon } from '../components/Icons/Icons'
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/Stats.module.css'
 
+type StatItemProps = {
+  value: number | string
+  unit: string
+  children?: ReactNode
+}
+
+const StatItem = ({ value, unit, children }: StatItemProps) => (
+  <div className={styles.item}>
+    <div className={styles.data}>
+      <span className={styles.value}>{value}</span>
+      <span className={styles.unit}>{unit}</span>
+    </div>
+    {children}
+  </div>
+)
+
 const Stats = () => {
   return (
-    <Layout>
+    <Layout container>
       <div className={styles.root}>
-        <div className={styles.container}></div>
+        <h2>Quelques petites stats :</h2>
+        <div className={styles.stats}>
+          <StatItem value={3} unit=" pays">
+            {' '}
+            <span className={styles.bigEmoji}>🇦🇿🇧🇪🇧🇻</span>
+          </StatItem>
+          <StatItem value={453} unit=" km"></StatItem>
+          <StatItem value={157} unit=" dates mangées"></StatItem>
+          <StatItem value={3} unit=" poulet capturés"></StatItem>
+        </div>
+        <footer className={styles.footer}>
+          <nav className={styles.nav}>
+            <ButtonLink href="/" title="Carte">
+              <MapIcon />
+            </ButtonLink>
+            <ButtonLink href="/list" title="Articles">
+              <ListIcon />
+            </ButtonLink>
+          </nav>
+        </footer>
       </div>
     </Layout>
   )
